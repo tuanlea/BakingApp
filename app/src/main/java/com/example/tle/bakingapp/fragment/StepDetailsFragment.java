b4dfc74c58e14e62c44d4b91615841e449a6a662
Tuan Le <tuan.le@altres.com>
2018-09-24 14:27:40 -1000

    Added video player

--- app/src/main/java/com/example/tle/bakingapp/fragment/StepDetailsFragment.java
+++ app/src/main/java/com/example/tle/bakingapp/fragment/StepDetailsFragment.java
@@ -1,10 +1,13 @@
 package com.example.tle.bakingapp.fragment;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,20 @@ import android.widget.TextView;
 
 import com.example.tle.bakingapp.R;
 import com.example.tle.bakingapp.model.Step;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
 
 public class StepDetailsFragment extends Fragment {
     Step step;
@@ -40,10 +57,40 @@ public class StepDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.step_details_fragment, container, false);
         TextView descriptionTv = view.findViewById(R.id.step_details_description_tv);
         String description = step.getDescription();
         descriptionTv.setText(description);
+
+        // 1. Create a default TrackSelector
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        DefaultTrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create the player
+        SimpleExoPlayer player =
+                ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
+
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
+                Util.getUserAgent(getContext(), "yourApplicationName")
+                );
+        // This is the MediaSource representing the media to be played.
+        String videoURL = step.getVideoURL();
+        if (!TextUtils.isEmpty(videoURL)) {
+            Uri mp4VideoUri = Uri.parse(videoURL);
+            MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(mp4VideoUri);
+            // Prepare the player with the source.
+            player.prepare(videoSource);
+
+            PlayerView playerView = view.findViewById(R.id.step_details_pv);
+            playerView.setPlayer(player);
+        }
+
         return view;
     }
 
